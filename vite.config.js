0ec7636a21cfd73453a942a863b058359f7d9674
nespera <msijuicebox@gmail.com>
2023-09-21 01:51:44 -0300

    fixed icon

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/dolar-calc/",
+  base: "/dolar-calc-svelte/",
 });
